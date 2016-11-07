7fd1a7574476d772025d64b056dbcd312b138da3
u4bi <rootcode10@gmail.com>
2016-11-07 05:30:05 +0000

    async module : series

--- server.js
+++ server.js
@@ -1,8 +1,19 @@
 var restify = require('restify');
+var async = require('async');
 
-function respond(req, res, next){
-    res.send('hello'+ req.params.name);
-    next();
+async.series([boy1,boy2,boy3],resultAsync);
+
+function boy1(callback){ callback(null,'boy1'); }
+function boy2(callback){ callback(null,'boy2'); }
+function boy3(callback){ callback(null,'boy3'); }
+function resultAsync(err, results){
+	if(err) console.log(err);
+	
+	console.log(results);
+	console.log('boy 1 : '+results[0]);
+}
+
+function respond(req, res){ res.send('hello'+ req.params.name);
 }
 
 var server = restify.createServer();
@@ -10,5 +21,5 @@ var server = restify.createServer();
 server.get('/hello/:name', respond);
 server.head('/hello/:name', respond);
 
-server.listen(8080);
-console.log('%s : url: %s', server.name, server.url);
+server.listen(3000);
+console.log('%s',server.url);
